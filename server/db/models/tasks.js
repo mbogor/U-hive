a12a4dbc57123de64f9e053099b6abeba6787c80
Ahertig <alyssa.hertig@gmail.com>
2016-02-29 17:03:04 -0500

    created the tasks model

---
+++ server/db/models/tasks.js
@@ -0,0 +1,44 @@
+'use strict';
+var mongoose = require('mongoose');
+
+var taskSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    category: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    }],
+    price: {
+        type: Number,
+        default: 0
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        default: '/default-image.png'
+    },
+    date: Date,
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    restrictedByCampus: {
+        type: Boolean
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    forSaleOrWanted: {
+        type: String,
+        required: true
+    }
+});
+
+mongoose.model('Task', taskSchema);
