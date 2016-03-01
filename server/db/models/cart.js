d884446da2cdc4e53369a7726581e2734045d1ae
Jisoo Shin <jisooxshin@gmail.com>
2016-03-01 18:03:33 -0500

    created getcompletedtasks static

--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -2,15 +2,58 @@
 
 var mongoose = require('mongoose');
 
+
+//user on cart is the buyer and user on a task is a seller
 var cartSchema = new mongoose.Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'User'},
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
   tasks: [{type: Schema.Types.ObjectId, ref: 'Task'}],
-  timeCreated: {
-    type: Date,
-    default: Date.now
-  }
+  timeCreated: { type: Date, default: Date.now }, 
+  paid: { type: Boolean, default: false },
+  processed: { type: Boolean, default: false }
 })
 
+
+											//cartTotal will be passed in from the front end so that we don't need to calculate the cart total here
+cartSchema.methods.processCheckout = function(cartTotal) {
+
+	var buyer = mongoose.model('User').findById(this.user);
+	var sellers = this.tasks.forEach(function(task){
+		mongoose.model('Task').findById(task._id)
+	})
+
+	if(buyer.uComb < cartTotal) throw new Error('You do not have enough credit.');
+
+	Promise.all([findBook, findChapter])
+  .spread(function (book, chapter) {
+    return book.removeChapter(chapter);
+  })
+  .then(function () {
+    res.sendStatus(204);
+  })
+   .then(null, notFound(next));
+
+
+
+
+
+	//2. update the buyers credit --remove money
+	mongoose.model('User').findById(this.user)
+	.then(function(user) {
+		if(user.uComb < cartTotal) throw new Error('You do not have enough credit.');
+		user.uComb = user.uComb - cartTotal;
+	})
+	
+
+	//1. update the sellers credit ++add money
+
+
+
+
+
+	//3. changed the cart status to true
+	this.processed = true;
+}
+
 mongoose.model('Cart', cartSchema);
+
+
--- server/db/models/user.js
+++ server/db/models/user.js
@@ -28,7 +28,8 @@ var schema = new mongoose.Schema({
         type: Boolean,
         default: false
     },
-    UComb: {
+    //UCombs is the $ amount you have on your account
+    uComb: {
         type: Number,
         default: 0
     },
@@ -58,6 +59,18 @@ var schema = new mongoose.Schema({
 
 //VIRTUAL FOR RATING
 
+
+
+
+
+
+
+
+
+//STATIC METHODS
+schema.statics.getCompletedTasks = function(userId) {
+    return mongoose.model('Task').find({user: userId, completed: true});
+}
 
 // method to remove sensitive information from user objects before sending them out
 schema.methods.sanitize =  function () {
