18018136f5e4fc525d27af95e21084e8b2a7ac20
Emma Bishop <emma.b.bishop@gmail.com>
2016-03-06 13:45:37 -0500

    adds route to get all collges

---
+++ server/app/routes/college/index.js
@@ -0,0 +1,14 @@
+'use strict';
+var router = require('express').Router();
+var mongoose = require('mongoose');
+module.exports = router;
+
+// get all colleges
+router.get('/', function(req, res, next){
+  mongoose.model('College').find()
+  .then(function(colleges){
+    console.log(colleges);
+    res.json(colleges);
+  })
+  .then(null, next);
+});
