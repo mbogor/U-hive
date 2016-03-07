4de1d110104020e7a1c151115503e25444dac584
Ahertig <alyssa.hertig@gmail.com>
2016-03-07 17:31:47 -0500

    created search filter for for sale items

--- browser/js/tasks/tasks.forsale.controller.js
+++ browser/js/tasks/tasks.forsale.controller.js
@@ -3,7 +3,13 @@
 app.controller('ForSaleCtrl', function($scope, forsale, $state, TaskFactory){
   $scope.forsale = forsale; //this is the list of tasks from the resolve block of tasksForSale
 
+  // $scope.categoryFilter = {
+  //   food: false, 
+  //   tutoring: false,
+  //   cleaning: false
+  // }
 
+  // $scope.categories = ['food', 'tutoring', 'cleaning']
 
   $scope.detailTransfer = function(id){
     console.log("live from detail transfer")
@@ -18,6 +24,16 @@ app.controller('ForSaleCtrl', function($scope, forsale, $state, TaskFactory){
 
 
 })
+
+// app.filter('categoryFilter', function(category) {
+//   return function(items) {
+//     var filtered = [];
+//     for(var i = 0; i < items.length; i++) {
+//       if(item.category === category) filtered.push(item)
+//     }
+//     return filtered;
+//   }
+// })
 
 
 app.controller('ItemCtrl', function($scope, task, $state, TaskFactory){
