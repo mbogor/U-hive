55e5261d45609c6f837313c05cde0d1bacf65ee4
Emma Bishop <emma.b.bishop@gmail.com>
2016-03-07 21:34:14 -0500

    Sets up local storage, adds items to local cart

--- browser/js/tasks/task.item.controller.js
+++ browser/js/tasks/task.item.controller.js
@@ -1,9 +1,31 @@
 'use strict';
 
-app.controller('TaskItem', function($scope){
+app.controller('TaskItem', function($scope, localStorageService, AuthService){
   $scope.expandTask =  true;
+
   $scope.toggleExpand = function(){
     $scope.expandTask=!$scope.expandTask;
+  }
+
+  $scope.addToCart = function(task){
+    if(AuthService.getLoggedInUser()) {
+      console.log('we have a user');
+      // do something different
+    }
+    if(!localStorageService.get('cart')){
+      localStorageService.set('cart', {tasks: [], timeCreated: Date.now()});
+    }
+
+    var existingCart = localStorageService.get('cart')
+    console.log('item is in cart', localStorageService.get('cart').tasks.indexOf(task._id)>-1);
+    existingCart.tasks.push(task._id);
+    localStorageService.set('cart', existingCart);
+
+  };
+
+  $scope.itemIsInCart = function(task){
+    // console.log('item is in cart:', localStorageService.get('cart').tasks.indexOf(task._id)>-1)
+    return localStorageService.get('cart').tasks.indexOf(task._id)>-1;
   }
 })
 .filter("trim", function(){
